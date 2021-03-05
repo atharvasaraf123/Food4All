b0b28d059869dff2418b72b52de003a0b4ac6291
vaibhavpallod <vaibhavpallod@gmail.com>
2021-03-05 22:19:11 +0530

    Indivisual view added

--- flutter_app/lib/BottomNavigation/IndividualScreen.dart
+++ flutter_app/lib/BottomNavigation/IndividualScreen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/IndivisualView/Donation.dart';
+import 'package:flutter_app/IndivisualView/body.dart';
+import 'package:like_button/like_button.dart';
+
+import 'Donate_Food.dart';
 
 class IndividualScreen extends StatefulWidget {
   @override
@@ -7,75 +12,113 @@ class IndividualScreen extends StatefulWidget {
 
 class _IndividualScreenState extends State<IndividualScreen> {
 
-  Widget _images(){
-    return Container(
-      child: Stack(
-        children: <Widget>[
-          Image.asset("images/shoe_blue.png",), //This
-          // should be a paged
-          // view.
-          Positioned(child:_getBackBtn(),top: 0,),
-          Positioned(child: FloatingActionButton(
-              elevation: 2,
-              child:Image.asset("images/heart_icon_disabled.png",
-                width: 30,
-                height: 30,),
-              backgroundColor: Colors.white,
-              onPressed: (){}
-          ),
-            bottom: 0,
-            right: 20,
-          ),
 
+  List<String> images = [
+    "images/ngoCharity2.png",
+    "images/fooddonationhand.png",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            AppBackground(),
+            Container(
+              height: 300,
+              child: Column(
+                children: [
+                  Body(donation: Donation(id: 0, images: images)),
+                  Container(
+                    padding: EdgeInsets.all(20.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Food Items : ',
+                          style: TextStyle(
+                              fontSize: 18.0, fontFamily: 'MontserratBold'),
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          'Paneer bhurji,Pav Bhaji,Pani puri',
+                          style: TextStyle(
+                              fontSize: 16.0, fontFamily: 'MontserratReg'),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            _getBackBtn(),
+            _likeButton(),
+
+          ],
+        ),
+      ),
+    );
+  }
+
+
+  _getBackBtn() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: Row(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.topLeft,
+            child: Material(
+              elevation: 10,
+              child: Padding(
+                padding: EdgeInsets.all(2),
+                child: IconButton(
+                  icon: Icon(
+                    Icons.arrow_back,
+                    color: Color(0xFFea9b72),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  iconSize: 24,
+                ),
+              ),
+              color: Colors.white,
+              shape: CircleBorder(),
+            ),
+          ),
+          Spacer(),
         ],
       ),
     );
   }
 
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-
-
-  _getBackBtn() {
-    return Positioned(
-      top: 35,
-      left: 25,
-      child: Icon(
-        Icons.arrow_back_ios,
-        color: Colors.white,
-      ),
-    );
-  }
-
-
-
-  GestureDetector buildSmallProductPreview(int index) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          selectedImage = index;
-        });
-      },
-      child: AnimatedContainer(
-        duration: defaultDuration,
-        margin: EdgeInsets.only(right: 15),
-        padding: EdgeInsets.all(8),
-        height: getProportionateScreenWidth(48),
-        width: getProportionateScreenWidth(48),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10),
-          border: Border.all(
-              color: kPrimaryColor.withOpacity(selectedImage == index ? 1 : 0)),
+  _likeButton() {
+    return   Positioned(
+      child:  Container(
+        width: 40,
+        height: 40,
+        child: Center(
+          child: LikeButton(
+            circleColor:
+            CircleColor(start: Color(0xFFF44336), end: Color(0xFFF44336)),
+            likeBuilder: (bool isLiked) {
+              return Icon(
+                Icons.favorite,
+                size: 30,
+                color: isLiked ? Colors.red : Colors.grey,
+              );
+            },
+          ),
         ),
-        child: Image.asset(widget.product.images[index]),
+        decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.white70),
       ),
+      top: 10,
+      right: 10,
     );
   }
-
-
-
 }
